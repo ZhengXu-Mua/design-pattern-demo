07b577dd5032dcff97a606bc6919d7c070aedd99
pomelo <260876834@qq.com>
2019-01-18 12:02:56 +0800

    修改了工厂方法，原本方法虽然减少了判断的次数，但是会多创建三个无用的对象，改用switch可能会增加判断的次数，但是减少了无用对象的创建

--- src/SimpleFactoryPattern/Caculator.java
+++ src/SimpleFactoryPattern/Caculator.java
@@ -6,22 +6,22 @@ import java.util.HashMap;
 简单工厂模式
  */
 public class Caculator {
-//    HashMap<String,Operation> operation = new HashMap<String,Operation>();
-//
-//    public Caculator(){
-//        this.operation.put("+",new OperationAdd());
-//        this.operation.put("-",new OperationSub());
-//        this.operation.put("*",new OperationMul());
-//        this.operation.put("/",new OperationDiv());
-//    }
+
+    //原本这个方法每次会创建四个对象，不好，没有必要，可以使用switch
+    //代替，虽然在极端情况下会增加判断次数，但是不需要多次创建无用对象
     public static Operation getOperationFactory(String operate){
         Operation oper = null;
-        HashMap<String,Operation> operation = new HashMap<String,Operation>();
-        operation.put("+",new OperationAdd());
-        operation.put("-",new OperationSub());
-        operation.put("*",new OperationMul());
-        operation.put("/",new OperationDiv());
-        oper = operation.get(operate);
+//        HashMap<String,Operation> operation = new HashMap<String,Operation>();
+//        operation.put("+",new OperationAdd());
+//        operation.put("-",new OperationSub());
+//        operation.put("*",new OperationMul());
+//        operation.put("/",new OperationDiv());
+        switch (operate){
+            case "+": oper = new OperationAdd();break;
+            case "-": oper = new OperationSub();break;
+            case "*": oper = new OperationMul();break;
+            case "/": oper = new OperationDiv();break;
+        }
         return oper;
     }
 }
--- src/SimpleFactoryPattern/OperationDiv.java
+++ src/SimpleFactoryPattern/OperationDiv.java
@@ -4,7 +4,11 @@ public class OperationDiv extends Operation{
     @Override
     public double GetResult() {
         double result = 0;
-        result = get_numberA()/get_numberB();
+        if(get_numberB()!=0){
+            result = get_numberA()/get_numberB();
+        }else{
+            System.out.println("除数不能为零");
+        }
         return result;
     }
 }
